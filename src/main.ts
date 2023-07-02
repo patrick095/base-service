2b3e71b8542b1d76ff5e47a8e92dc322e8bf79c4
Patrick Chaves <patrickchaves@MacBook-Pro-de-Patrick.local>
2023-07-01 22:05:57 -0300

    Correções Prettier

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.PORT;
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(PORT);
+    const PORT = process.env.PORT;
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(PORT);
 }
 bootstrap();
